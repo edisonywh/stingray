2e4018183ab86a1c05b1eddd9c3b0deb8aa29368
Edison Yap <edisonywh@gmail.com>
2020-07-14 14:18:13 +0800

    Make Story properties nullable

--- lib/model/story.dart
+++ lib/model/story.dart
@@ -34,26 +34,28 @@ class Story {
   }
 
   factory Story.fromMap(Map<String, dynamic> json) => Story(
-        by: json["by"],
-        descendants: json["descendants"],
         id: json["id"],
-        kids: List<int>.from(json["kids"].map((x) => x)),
-        score: json["score"],
-        time: json["time"],
-        title: json["title"],
-        type: json["type"],
-        url: json["url"],
+        by: json["by"] == null ? null : json["by"],
+        descendants: json["descendants"] == null ? null : json["descendants"],
+        kids: json["kids"] == null
+            ? null
+            : List<int>.from(json["kids"].map((x) => x)),
+        score: json["score"] == null ? null : json["score"],
+        time: json["time"] == null ? null : json["time"],
+        title: json["title"] == null ? null : json["title"],
+        type: json["type"] == null ? null : json["type"],
+        url: json["url"] == null ? null : json["url"],
       );
 
   Map<String, dynamic> toMap() => {
-        "by": by,
-        "descendants": descendants,
         "id": id,
-        "kids": List<dynamic>.from(kids.map((x) => x)),
-        "score": score,
-        "time": time,
-        "title": title,
-        "type": type,
-        "url": url,
+        "by": by == null ? null : by,
+        "descendants": descendants == null ? null : descendants,
+        "kids": kids == null ? null : List<dynamic>.from(kids.map((x) => x)),
+        "score": score == null ? null : score,
+        "time": time == null ? null : time,
+        "title": title == null ? null : title,
+        "type": type == null ? null : type,
+        "url": url == null ? null : url,
       };
 }
