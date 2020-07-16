c58bd0274e9ce68da534d2ec7c00e06bb9adb233
Edison Yap <edisonywh@gmail.com>
2020-07-16 15:44:28 +0800

    Change Loading borderRadius

--- lib/component/loading_comment.dart
+++ lib/component/loading_comment.dart
@@ -18,23 +18,57 @@ class LoadingComment extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Container(width: 80, height: 10, color: Colors.white24),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                width: 80,
+                height: 10,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Container(height: 10, color: Colors.white24),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                height: 10,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Container(height: 10, color: Colors.white24),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                height: 10,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Container(height: 10, color: Colors.white24),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white24,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                height: 10,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Container(height: 10, color: Colors.white24),
+              child: FractionallySizedBox(
+                widthFactor: 0.7,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white24,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  height: 10,
+                ),
+              ),
             ),
           ],
         ),
