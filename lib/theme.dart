2e0aa8749a40a9465aa45e7a3dc4a3a4c682f785
Edison Yap <edisonywh@gmail.com>
2020-07-20 19:40:39 +0800

    Add Launcher Icon

--- lib/theme.dart
+++ lib/theme.dart
@@ -8,10 +8,10 @@ final StateProvider themeProvider = StateProvider((ref) {
 ThemeData trueBlackTheme = ThemeData.dark().copyWith(
   scaffoldBackgroundColor: Colors.black,
   cardColor: Colors.white,
-  primaryColor: Color(0xFFFFC98A),
-  accentColor: Color(0xFFFFC98A),
-  indicatorColor: Color(0xFFFFC98A),
-  toggleableActiveColor: Color(0xFFFFC98A),
+  primaryColor: Color(0xFFFFA826),
+  accentColor: Color(0xFFFFA826),
+  indicatorColor: Color(0xFFFFA826),
+  toggleableActiveColor: Color(0xFFFFA826),
   cardTheme: ThemeData().cardTheme.copyWith(
         color: Color(0xFF1C1C1C),
       ),
@@ -23,10 +23,10 @@ ThemeData trueBlackTheme = ThemeData.dark().copyWith(
 
 ThemeData darkTheme = ThemeData.dark().copyWith(
   scaffoldBackgroundColor: Color(0xFF1F1F1F),
-  primaryColor: Color(0xFFFFC98A),
-  accentColor: Color(0xFFFFC98A),
-  indicatorColor: Color(0xFFFFC98A),
-  toggleableActiveColor: Color(0xFFFFC98A),
+  primaryColor: Color(0xFFFFA826),
+  accentColor: Color(0xFFFFA826),
+  indicatorColor: Color(0xFFFFA826),
+  toggleableActiveColor: Color(0xFFFFA826),
   cardTheme: ThemeData().cardTheme.copyWith(
         color: Color(0xFF1C1C1C),
       ),
@@ -37,13 +37,13 @@ ThemeData darkTheme = ThemeData.dark().copyWith(
 );
 
 ThemeData lightTheme = ThemeData.light().copyWith(
-  primaryColor: Color(0xFFF99602),
-  accentColor: Color(0xFFF99602),
-  indicatorColor: Color(0xFFF99602),
-  toggleableActiveColor: Color(0xFFF99602),
+  primaryColor: Color(0xFFFFA826),
+  accentColor: Color(0xFFFFA826),
+  indicatorColor: Color(0xFFFFA826),
+  toggleableActiveColor: Color(0xFFFFA826),
   tabBarTheme: ThemeData().tabBarTheme.copyWith(
         unselectedLabelColor: Colors.grey,
-        labelColor: Color(0xFFF99602),
+        labelColor: Color(0xFFFFA826),
       ),
   appBarTheme: ThemeData().appBarTheme.copyWith(
         color: Colors.white,
